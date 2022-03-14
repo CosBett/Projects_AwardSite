0e67706edf3cd651723e1e418c7f3825f5be18a6
CosBett <cosmasbett9@gmail.com>
2022-03-14 15:37:17 +0300

    endpoint for homepage posts display

--- awardApp/views.py
+++ awardApp/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render,redirect
-from .forms import SignupForms
+from .forms import PostForm, SignupForms
 from django.contrib.auth import login, authenticate
-
+from .models import Profile, Post, Rating
+import random
 
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post= form.save(commit=False)
+            post.user =request.user
+            post.save()
+    else:
+        form = PostForm()
 
-    return render(request, 'index.html')
+    try:
+        post = Post.objects.all()
+        posts =posts[::-1]
+        displayed_post = random.ranint(0,len(posts)-1)
+        random_posts = posts[displayed_post]
+    except Post.DoesNotExist:
+        posts = None    
+    homepage = {'posts':posts, 'form':form,'random_posts':random_posts}
+    return render(request, 'index.html', homepage)
 
 def signup(request):
     if request.method == 'POST':
