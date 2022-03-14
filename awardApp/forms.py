46154fbab97af6d5261b62b62d06856cf99c7089
CosBett <cosmasbett9@gmail.com>
2022-03-14 13:17:05 +0300

    create update profile form to forms.py

--- awardApp/forms.py
+++ awardApp/forms.py
@@ -17,6 +17,12 @@ class UpdateUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email')
+        
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'location', 'profile_picture', 'bio', 'contact']
+
 
 class PostForm(forms.ModelForm):
     photo = ImageField(label='')
