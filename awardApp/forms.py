cefdf63c9efc559734c8f5a416dbd137cf2fd6e9
CosBett <cosmasbett9@gmail.com>
2022-03-14 13:16:06 +0300

    add postform and update it

--- awardApp/forms.py
+++ awardApp/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from pyuploadcare.dj.forms import ImageField
+from .models import Post, Profile, Rating
 
 class SignupForms(UserCreationForm):
     email = forms.EmailField(max_length=300, help_text=' Input Required. Input a valid email address.')
@@ -10,9 +12,16 @@ class SignupForms(UserCreationForm):
         fields = ('username', 'email', 'password1', 'password2')
 
 class UpdateUserForm(forms.ModelForm):
-    email = forms.EmailField(max_length=254, help_text='Required. In a valid email address.')
+    email = forms.EmailField(max_length=300, help_text='Required. In a valid email address.')
 
     class Meta:
         model = User
         fields = ('username', 'email')
+
+class PostForm(forms.ModelForm):
+    photo = ImageField(label='')
+
+    class Meta:
+        model = Post
+        fields = ('photo', 'title', 'url', 'description', 'technologies',)
 
