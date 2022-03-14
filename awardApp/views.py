0f0474e2b86a5278d804fdea0a328aec63afbcd6
CosBett <cosmasbett9@gmail.com>
2022-03-14 06:20:06 +0300

    create registration view function and endpoint

--- awardApp/views.py
+++ awardApp/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import SignupForms
+from django.contrib.auth import login, authenticate
+
+
 
 # Create your views here.
 def index(request):
@@ -6,6 +10,19 @@ def index(request):
     return render(request, 'index.html')
 
 def signup(request):
+    if request.method == 'POST':
+        form = SignupForms(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form =  SignupForms     
 
-    return render(request, 'registration /signup.html')
+    signup_context = {'form':form}
+    
+    return render(request, 'registration /signup.html', signup_context)
 
--- awardProject/settings.py
+++ awardProject/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'awardApp',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -115,6 +116,10 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 # Default primary key field type
+LOGIN_REDIRECT_URL = 'index'
+
+LOGOUT_REDIRECT_URL = 'index'
+
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
