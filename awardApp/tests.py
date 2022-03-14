8a4c8c106523e866450c4a0316194da3d46c1b10
CosBett <cosmasbett9@gmail.com>
2022-03-14 10:09:08 +0300

    write tests for post model class

--- awardApp/tests.py
+++ awardApp/tests.py
@@ -14,3 +14,32 @@ class TestProfile(TestCase):
 
     def test_delete_user(self):
         self.user.delete()
+
+class TestPost(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id=1, username='Prime')
+        self.post = Post.objects.create(id=1, title='Post testing', photo='image.png', description='This is test description',
+        user=self.user, url='http://urltest.com')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))
+
+    def test_save_post(self):
+        self.post.save_post()
+        post = Post.objects.all()
+        self.assertTrue(len(post) > 0)
+
+    def test_get_posts(self):
+        self.post.save()
+        posts = Post.all_posts()
+        self.assertTrue(len(posts) > 0)
+
+    def test_search_post(self):
+        self.post.save()
+        post = Post.search_project('test')
+        self.assertTrue(len(post) > 0)
+
+    def test_delete_post(self):
+        self.post.delete_post()
+        post = Post.search_project('test')
+        self.assertTrue(len(post) < 1)
